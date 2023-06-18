b10add66218f91dfee577a0c3e3bb1222646c698
9ahuang <andyhuang0611@gmail.com>
2023-06-18 20:46:58 +1200

    Fixed Login/Register pages input text color

--- test.php
+++ test.php
@@ -1,61 +1,83 @@
 <!DOCTYPE html>
-<html>
+<html lang="EN-US">
 
 <head>
-  <title>Login Page</title>
-  <style>
-    body {
-      font-family: Arial, sans-serif;
-    }
-
-    .container {
-      width: 300px;
-      margin: 0 auto;
-      padding: 20px;
-      border: 1px solid #ccc;
-      border-radius: 5px;
-      background-color: #f2f2f2;
-    }
-
-    .container h2 {
-      text-align: center;
-    }
-
-    .container input[type="text"],
-    .container input[type="password"] {
-      width: 100%;
-      padding: 10px;
-      margin-bottom: 10px;
-      border: 1px solid #ccc;
-      border-radius: 3px;
-    }
-
-    .container input[type="submit"] {
-      width: 100%;
-      padding: 10px;
-      border: none;
-      border-radius: 3px;
-      background-color: dodgerblue;
-      color: #fff;
-      cursor: pointer;
-    }
-  </style>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, inital-scale=1.0">
+  <title>HBHS booking website</title>
+  <link rel="stylesheet" href="style.css">
+  <script src="https://unpkg.com/feather-icons"></script>
 </head>
 
 <body>
-  <div class="container">
-    <h2>Login</h2>
-    <form>
-      <input type="text" placeholder="Username" required>
-      <input type="password" placeholder="Password" required>
-      <input type="submit" value="Login">
-    </form>
+  <div class="wrapper">
+    <header>
+      <img src="images/logo place holder.png" class="logo">
+    </header>
+    <div id="wrapperNav">
+      <nav>
+        <ul class="navList">
+          <li class="navListItem"><a class="navLinks" href="#">Book</a></li>
+          <li class="navListItem"><a class="navLinks" href="#">About</a></li>
+          <li class="navListItem"><a class="navLinks" href="index.html">Home</a></li>
+        </ul>
+
+        <i data-feather="user" class="user-pic" onclick="toggleMenu()"></i>
+
+        <div class="sub-menu-wrap" id="subMenu">
+          <div class="sub-menu">
+            <div class="user-info">
+              <i data-feather="user" class="user-info-icon"></i>
+              <h3>USER NAME</h3>
+            </div>
+
+            <hr>
+
+            <a href="#" class="sub-menu-link">
+              <i data-feather="user" class="user-info-icon"></i>
+              <p>profile</p>
+              <span>></span>
+            </a>
+
+            <a href="#" class="sub-menu-link">
+              <i data-feather="help-circle" class="user-info-icon"></i>
+              <p>Help</p>
+              <span>></span>
+            </a>
+            <a href="#" class="sub-menu-link">
+              <i data-feather="log-out" class="user-info-icon"></i>
+              <p>Logout</p>
+              <span>></span>
+            </a>
+          </div>
+        </div>
+    </div>
+    </nav>
+
+    <main>
+      <?php
+       echo "hello world"
+      ?>
+
+    </main>
   </div>
 
-  <?php 
-  
-  ?>
+  <footer>
+    <h3>Contact Us</h3>
+    <i data-feather="instagram"></i>
+  </footer>
 
+  <script>
+    feather.replace();
+  </script>
+
+  <script>
+    let subMenu = document.getElementById("subMenu");
+
+    function toggleMenu() {
+      subMenu.classList.toggle("open-menu")
+    }
+  </script>
 </body>
 
 </html>
